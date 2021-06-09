b2cd20bd48a9d30d43d86ee1af3573e886c40530
rishi22-hub <rishichaitanyatiwari22@gmail.com>
2021-06-09 11:39:20 +0530

    solving sum bugs

--- cal.js
+++ cal.js
@@ -1,6 +1,24 @@
 var display=document.getElementById('scr');
 var buttons=document.querySelectorAll('button');
 var disp1='';
+
+const isValid = (str = '') => {
+    const map=new Map();
+ 
+   map.set('(',')');
+    const b=[];
+   for(let i=0;i<str.length;i++){
+      if(map.has(str.charAt(i))){
+         b.push(str.charAt(i));
+      } else{
+         let pop=b.pop();
+         if(map.get(pop)!==str.charAt(i)){
+            return false;
+         }
+      };
+   };
+   return b.length===0;
+}
 for(items of buttons){
     items.addEventListener('click',(e)=>{
       buttontext=e.target.innerText;
@@ -9,7 +27,9 @@ for(items of buttons){
             disp1='';
         }
         else if(buttontext=="="){
+            if(!isValid(disp1))alert('check input and correct it');
             display.value=eval(disp1);
+            disp1=display.value;
         }
         else {disp1+=buttontext;display.value=disp1;}
     }
