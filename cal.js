d14bf3c75c7a6c62f084729918e275a5df74342c
rishi22-hub <rishichaitanyatiwari22@gmail.com>
2021-06-11 13:44:46 +0530

    added backspace button

--- cal.js
+++ cal.js
@@ -1,36 +1,40 @@
 var display=document.getElementById('scr');
 var buttons=document.querySelectorAll('button');
 var disp1='';
+var isValid = function (s) {
 
-const isValid = (str = '') => {
-    const map=new Map();
+   const obj = {
+     "(": ")"
+   }
  
-   map.set('(',')');
-    const b=[];
-   for(let i=0;i<str.length;i++){
-      if(map.has(str.charAt(i))){
-         b.push(str.charAt(i));
-      } else{
-         let pop=b.pop();
-         if(map.get(pop)!==str.charAt(i)){
-            return false;
-         }
-      };
-   };
-   return b.length===0;
-}
+   const stack = [];
+ 
+   for (const paran of s) {
+     if (obj.hasOwnProperty(paran)) {
+       stack.push(paran)
+     } else {
+       const closeParan = stack.pop();
+       if (paran !== obj[closeParan]) {
+         return false;
+       }
+     }
+   }
+ 
+   return stack.length === 0;
+ };
 for(items of buttons){
     items.addEventListener('click',(e)=>{
       buttontext=e.target.innerText;
-        if(buttontext=='C'){
+        if(buttontext==='C'){
             display.value="";
             disp1='';
         }
-        else if(buttontext=="="){
-            if(!isValid(disp1))alert('check input and correct it');
+        else if(buttontext==="="){
+            
             display.value=eval(disp1);
             disp1=display.value;
         }
+        else if(buttontext==='backspace'){var str=disp1.slice(0,-1);disp1=str;display.value=disp1;}
         else {disp1+=buttontext;display.value=disp1;}
     }
     )
